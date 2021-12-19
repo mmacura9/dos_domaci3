7857c804945b47716ecc85af65a919d6133e13c4
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2021-12-19 02:19:01 +0100

    First commit

---
+++ domaci3_18_261/zad1.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 21:23:16 2021
+
+@author: mm180261d
+"""
+from pylab import *
+from skimage import io
+from skimage import filters
+from skimage import color
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def canny_edge_detection(img_in: np.array, sigma: float, threshold_low: float, threshold_high: float) -> np.array:
+    # filtering the input image
+    img = filters.gaussian(img_in, sigma=sigma, truncate=3)
+    plt.figure()
+    io.imshow(img)
+    
+    # gradients
+    sobel_vertical = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])/8
+    sobel_horizontal = np.array([[-1, 0 , 1], [-2, 0, 2], [-1, 0, 1]])/8
+    
+    gradient_vertical = ndimage.correlate(img, sobel_vertical)
+    gradient_horizontal = ndimage.correlate(img, sobel_horizontal)
+    # vertical gradient
+    plt.figure()
+    io.imshow(gradient_vertical, cmap='gray')
+    
+    # horizontal gradient
+    plt.figure()
+    io.imshow(gradient_horizontal, cmap='gray')
+    
+    magnitude = np.sqrt(gradient_vertical**2 + gradient_horizontal**2)
+    angle = np.arctan2(gradient_vertical, gradient_horizontal)
+    
+    # magnitude
+    plt.figure()
+    io.imshow(magnitude, cmap='gray')
+    
+    # angle
+    plt.figure()
+    io.imshow(angle, cmap='gray')
+    
+    # quantization
+    radian225 = math.radians(22.5)
+    radian675 = math.radians(67.5)
+    radian1125 = math.radians(112.5)
+    radian1575 = math.radians(157.5)
+    
+    radian_225 = math.radians(-22.5)
+    radian_675 = math.radians(-67.5)
+    radian_1125 = math.radians(-112.5)
+    radian_1575 = math.radians(-157.5)
+    
+    horizontal = ((angle > radian_225) * (angle < radian225)) + (angle < radian_1575) + (angle > radian1575)
+    plt.figure()
+    io.imshow(horizontal*1.)
+    
+    vertical = ((angle > radian675) * (angle < radian1125)) + ((angle < radian_675) * (angle > radian_1125))
+    plt.figure()
+    io.imshow(vertical*1.)
+    
+    diag45 = ((angle > radian1125) * (angle < radian1575)) + ((angle < radian_225) * (angle > radian_675))
+    plt.figure()
+    io.imshow(diag45*1.)
+    
+    diag_45 = ((angle > radian225) * (angle < radian675)) + ((angle < radian_1125) * (angle > radian_1575))
+    plt.figure()
+    io.imshow(diag_45*1.)
+    
+if __name__ == "__main__":
+    img_in = imread('../sekvence/clocks/clock1.png')
+    img_in = color.rgb2gray(color.rgba2rgb(img_in))
+    canny_edge_detection(img_in, 4, 2, 5)
+    # plt.figure()
+    # io.imshow(img_in)
