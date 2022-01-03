374247642fb71d81fe08d3df80dda693847c90bd
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2022-01-03 16:55:39 +0100

    Presentation update

--- domaci3_18_261/zad1.py
+++ domaci3_18_261/zad1.py
@@ -355,185 +355,185 @@ def extract_time(img_in: np.array) -> tuple:
         hours = 12
     return hours, minutes
 
-def extract_time_bonus(img_in: np.array) -> tuple:
-    canny = canny_edge_detection(img_in, 0.2, 0.2, 0.55)
+# def extract_time_bonus(img_in: np.array) -> tuple:
+#     canny = canny_edge_detection(img_in, 0.2, 0.2, 0.55)
     
-    canny_seconds = np.copy(canny)
-    canny_seconds[canny_seconds>np.max(canny_seconds)*3/4] = 0
+#     canny_seconds = np.copy(canny)
+#     canny_seconds[canny_seconds>np.max(canny_seconds)*3/4] = 0
     
-    plt.figure(figsize=(12, 9), dpi=80)
-    io.imshow(canny, cmap='gray')
+#     plt.figure(figsize=(12, 9), dpi=80)
+#     io.imshow(canny, cmap='gray')
     
-    ok=False
-    left = 0
-    s = 0
-    for i in range(canny.shape[0]):
-        if np.sum(canny[i, :]) != 0 and ok == False:
-            s = i
-            ok = True
-        if np.sum(canny[i, :]) == 0 and ok == True:
-            canny = canny[s:i, :]
-            break
-    ok=False
-    left = 0
-    s = 0
-    for i in range(canny.shape[1]):
-        if np.sum(canny[:, i]) != 0 and ok == False:
-            ok = True
-            s = i
-        if np.sum(canny[:, i]) == 0 and ok == True:
-            st = i
-            canny = canny[:, s:i]
-            break
-    plt.figure(figsize=(12, 9), dpi=80)
-    io.imshow(canny, cmap='gray')
+#     ok=False
+#     left = 0
+#     s = 0
+#     for i in range(canny.shape[0]):
+#         if np.sum(canny[i, :]) != 0 and ok == False:
+#             s = i
+#             ok = True
+#         if np.sum(canny[i, :]) == 0 and ok == True:
+#             canny = canny[s:i, :]
+#             break
+#     ok=False
+#     left = 0
+#     s = 0
+#     for i in range(canny.shape[1]):
+#         if np.sum(canny[:, i]) != 0 and ok == False:
+#             ok = True
+#             s = i
+#         if np.sum(canny[:, i]) == 0 and ok == True:
+#             st = i
+#             canny = canny[:, s:i]
+#             break
+#     plt.figure(figsize=(12, 9), dpi=80)
+#     io.imshow(canny, cmap='gray')
     
-    canny[canny<np.max(canny)*3/4] = 0
+#     canny[canny<np.max(canny)*3/4] = 0
     
-    plt.figure(figsize=(12, 9), dpi=80)
-    io.imshow(canny, cmap='gray')
+#     plt.figure(figsize=(12, 9), dpi=80)
+#     io.imshow(canny, cmap='gray')
     
-    [out, angles, distances] = skimage.transform.hough_line(canny)
-    [intensity, peak_angles, peak_distances] = skimage.transform.hough_line_peaks(out, angles=angles, dists=distances, min_distance=20, threshold=0.4*amax(out), num_peaks=5)
+#     [out, angles, distances] = skimage.transform.hough_line(canny)
+#     [intensity, peak_angles, peak_distances] = skimage.transform.hough_line_peaks(out, angles=angles, dists=distances, min_distance=20, threshold=0.4*amax(out), num_peaks=5)
     
-    line_lengths = []
+#     line_lengths = []
     
-    fix, axes = plt.subplots(1, 1, figsize=(20, 8))
-    axes.imshow(canny, cmap=plt.cm.gray)
-    axes.set_title('Input image with detected lines')
-    origin = np.array((0, img_in.shape[1]))
-    max1 = -1
-    ang1 = -1
-    right_half1 = False
-    top_half1 = False
-    max2 = -1
-    ang2 = -1
-    right_half2 = False
-    top_half2 = False
+#     fix, axes = plt.subplots(1, 1, figsize=(20, 8))
+#     axes.imshow(canny, cmap=plt.cm.gray)
+#     axes.set_title('Input image with detected lines')
+#     origin = np.array((0, img_in.shape[1]))
+#     max1 = -1
+#     ang1 = -1
+#     right_half1 = False
+#     top_half1 = False
+#     max2 = -1
+#     ang2 = -1
+#     right_half2 = False
+#     top_half2 = False
     
-    for _, angle, dist in zip(intensity, peak_angles, peak_distances):
-        y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
-        lines = get_line_segments(canny, np.array([angle, dist]), 60, 20, 1)
-        for line in lines:
-            if not line[0][0] in range(round(canny.shape[0]/2)-30, round(canny.shape[0]/2)+30) and not line[1][0] in range(round(canny.shape[0]/2)-30, round(canny.shape[0]/2)+30):
-                if not line[0][1] in range(round(canny.shape[1]/2)-30, round(canny.shape[1]/2)+30) and not line[1][1] in range(round(canny.shape[1]/2)-30, round(canny.shape[1]/2)+30):
-                    break
-            right_half = False
-            top_half = False
-            if (line[0][1] + line[1][1])/2 > canny.shape[1]/2:
-                right_half = True
-            if (line[0][0] + line[1][0])/2 < canny.shape[0]/2:
-                top_half = True
-            line_length = sqrt((line[0][0]- line[1][0])**2 + (line[0][1]-line[1][1])**2)
-            line_lengths = line_lengths + [line_length]
-            if max1 == -1:
-                max1 = line_length
-                ang1 = angle
-                right_half1 = right_half
-                top_half1 = top_half
-            else:
-                if max1 > line_length and max2 < line_length:
-                    max2 = line_length
-                    ang2 = angle
-                    right_half2 = right_half
-                    top_half2 = top_half
-            if line_length> max1:
-                max2 = max1
-                ang2 = ang1
-                right_half2 = right_half1
-                top_half2 = top_half1
-                max1 = line_length
-                ang1 = angle
-                right_half1 = right_half
-                top_half1 = top_half
-        axes.plot(origin, (y0, y1), '-r')
-    if max2 == -1:
-        max2 = max1
-        ang2 = ang1
-        right_half2 = right_half1
-        top_half2 = top_half1
-    axes.set_xlim(origin)
-    axes.set_ylim((img_in.shape[0], 0))
-    # axes.set_axis_off()
+#     for _, angle, dist in zip(intensity, peak_angles, peak_distances):
+#         y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+#         lines = get_line_segments(canny, np.array([angle, dist]), 60, 20, 1)
+#         for line in lines:
+#             if not line[0][0] in range(round(canny.shape[0]/2)-30, round(canny.shape[0]/2)+30) and not line[1][0] in range(round(canny.shape[0]/2)-30, round(canny.shape[0]/2)+30):
+#                 if not line[0][1] in range(round(canny.shape[1]/2)-30, round(canny.shape[1]/2)+30) and not line[1][1] in range(round(canny.shape[1]/2)-30, round(canny.shape[1]/2)+30):
+#                     break
+#             right_half = False
+#             top_half = False
+#             if (line[0][1] + line[1][1])/2 > canny.shape[1]/2:
+#                 right_half = True
+#             if (line[0][0] + line[1][0])/2 < canny.shape[0]/2:
+#                 top_half = True
+#             line_length = sqrt((line[0][0]- line[1][0])**2 + (line[0][1]-line[1][1])**2)
+#             line_lengths = line_lengths + [line_length]
+#             if max1 == -1:
+#                 max1 = line_length
+#                 ang1 = angle
+#                 right_half1 = right_half
+#                 top_half1 = top_half
+#             else:
+#                 if max1 > line_length and max2 < line_length:
+#                     max2 = line_length
+#                     ang2 = angle
+#                     right_half2 = right_half
+#                     top_half2 = top_half
+#             if line_length> max1:
+#                 max2 = max1
+#                 ang2 = ang1
+#                 right_half2 = right_half1
+#                 top_half2 = top_half1
+#                 max1 = line_length
+#                 ang1 = angle
+#                 right_half1 = right_half
+#                 top_half1 = top_half
+#         axes.plot(origin, (y0, y1), '-r')
+#     if max2 == -1:
+#         max2 = max1
+#         ang2 = ang1
+#         right_half2 = right_half1
+#         top_half2 = top_half1
+#     axes.set_xlim(origin)
+#     axes.set_ylim((img_in.shape[0], 0))
+#     # axes.set_axis_off()
     
-    plt.show()
-    ang_minutes = ang1
-    if ang1>0:
-        if (top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1 >= deg2rad(45)and ang1 <= deg2rad(90)) or (ang1 >= deg2rad(-90)and ang1<=deg2rad(-45)))):
-            ang_minutes = ang1
-        else:
-            ang_minutes = deg2rad(180) + ang1
-    else:
-        if (not top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1>=deg2rad(45) and ang1<=deg2rad(90)) or (ang1 >= deg2rad(-90) and ang1 <= deg2rad(-45)))):
-            ang_minutes = deg2rad(180) + ang1
-        else:
-            ang_minutes = deg2rad(360) + ang1
+#     plt.show()
+#     ang_minutes = ang1
+#     if ang1>0:
+#         if (top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1 >= deg2rad(45)and ang1 <= deg2rad(90)) or (ang1 >= deg2rad(-90)and ang1<=deg2rad(-45)))):
+#             ang_minutes = ang1
+#         else:
+#             ang_minutes = deg2rad(180) + ang1
+#     else:
+#         if (not top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1>=deg2rad(45) and ang1<=deg2rad(90)) or (ang1 >= deg2rad(-90) and ang1 <= deg2rad(-45)))):
+#             ang_minutes = deg2rad(180) + ang1
+#         else:
+#             ang_minutes = deg2rad(360) + ang1
     
-    minutes = round(ang_minutes/deg2rad(360)*60)
-    ang_hours = ang2
-    if ang2>0:
-        if (top_half2 and ang2 >= deg2rad(-45) and ang2 <= deg2rad(45)) or ((right_half2 and ((ang2 >= deg2rad(45)and ang2 <= deg2rad(90)) or (ang2 >=deg2rad(-90) and ang2 <= deg2rad(-45))))):
-            ang_hours = ang2
-        else:
-            ang_hours = deg2rad(180) + ang2
-    else:
-        if (not top_half2 and ang2 >= deg2rad(-45) and ang2 <= deg2rad(45)) or (right_half2 and ((ang2 >= deg2rad(45) and ang2 <= deg2rad(90)) or (ang2 >= deg2rad(-90) and ang2 <= deg2rad(-45)))):
-            ang_hours = deg2rad(180) + ang2
-        else:
-            ang_hours = deg2rad(360) + ang2
+#     minutes = round(ang_minutes/deg2rad(360)*60)
+#     ang_hours = ang2
+#     if ang2>0:
+#         if (top_half2 and ang2 >= deg2rad(-45) and ang2 <= deg2rad(45)) or ((right_half2 and ((ang2 >= deg2rad(45)and ang2 <= deg2rad(90)) or (ang2 >=deg2rad(-90) and ang2 <= deg2rad(-45))))):
+#             ang_hours = ang2
+#         else:
+#             ang_hours = deg2rad(180) + ang2
+#     else:
+#         if (not top_half2 and ang2 >= deg2rad(-45) and ang2 <= deg2rad(45)) or (right_half2 and ((ang2 >= deg2rad(45) and ang2 <= deg2rad(90)) or (ang2 >= deg2rad(-90) and ang2 <= deg2rad(-45)))):
+#             ang_hours = deg2rad(180) + ang2
+#         else:
+#             ang_hours = deg2rad(360) + ang2
     
-    hours = int(floor(ang_hours/deg2rad(360)*12))
-    if minutes == 60:
-        hours = hours+1
-        minutes=0
-    if hours == 0:
-        hours = 12
+#     hours = int(floor(ang_hours/deg2rad(360)*12))
+#     if minutes == 60:
+#         hours = hours+1
+#         minutes=0
+#     if hours == 0:
+#         hours = 12
 
-    origin = np.array((0, img_in.shape[1]))
-    max1 = -1
-    ang1 = -1
-    right_half1 = False
-    top_half1 = False
+#     origin = np.array((0, img_in.shape[1]))
+#     max1 = -1
+#     ang1 = -1
+#     right_half1 = False
+#     top_half1 = False
     
-    for _, angle, dist in zip(intensity, peak_angles, peak_distances):
-        y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
-        lines = get_line_segments(canny_seconds, np.array([angle, dist]), 30, 30, 3)
-        for line in lines:
-            if not line[0][0] in range(round(canny_seconds.shape[0]/2)-30, round(canny_seconds.shape[0]/2)+30) and not line[1][0] in range(round(canny_seconds.shape[0]/2)-30, round(canny_seconds.shape[0]/2)+30):
-                if not line[0][1] in range(round(canny_seconds.shape[1]/2)-30, round(canny_seconds.shape[1]/2)+30) and not line[1][1] in range(round(canny_seconds.shape[1]/2)-30, round(canny_seconds.shape[1]/2)+30):
-                    break
-            right_half = False
-            top_half = False
-            if (line[0][1] + line[1][1])/2 > canny_seconds.shape[1]/2:
-                right_half = True
-            if (line[0][0] + line[1][0])/2 < canny_seconds.shape[0]/2:
-                top_half = True
-            line_length = sqrt((line[0][0]- line[1][0])**2 + (line[0][1]-line[1][1])**2)
-            line_lengths = line_lengths + [line_length]
-            if line_length> max1:
-                max1 = line_length
-                ang1 = angle
-                right_half1 = right_half
-                top_half1 = top_half
-        # axes.plot(origin, (y0, y1), '-r')
-    ang_seconds = ang1
-    if ang1>0:
-        if (top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1 >= deg2rad(45)and ang1 <= deg2rad(90)) or (ang1 >= deg2rad(-90)and ang1<=deg2rad(-45)))):
-            ang_seconds = ang1
-        else:
-            ang_seconds = deg2rad(180) + ang1
-    else:
-        if (not top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1>=deg2rad(45) and ang1<=deg2rad(90)) or (ang1 >= deg2rad(-90) and ang1 <= deg2rad(-45)))):
-            ang_seconds = deg2rad(180) + ang1
-        else:
-            ang_seconds = deg2rad(360) + ang1
+#     for _, angle, dist in zip(intensity, peak_angles, peak_distances):
+#         y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+#         lines = get_line_segments(canny_seconds, np.array([angle, dist]), 30, 30, 3)
+#         for line in lines:
+#             if not line[0][0] in range(round(canny_seconds.shape[0]/2)-30, round(canny_seconds.shape[0]/2)+30) and not line[1][0] in range(round(canny_seconds.shape[0]/2)-30, round(canny_seconds.shape[0]/2)+30):
+#                 if not line[0][1] in range(round(canny_seconds.shape[1]/2)-30, round(canny_seconds.shape[1]/2)+30) and not line[1][1] in range(round(canny_seconds.shape[1]/2)-30, round(canny_seconds.shape[1]/2)+30):
+#                     break
+#             right_half = False
+#             top_half = False
+#             if (line[0][1] + line[1][1])/2 > canny_seconds.shape[1]/2:
+#                 right_half = True
+#             if (line[0][0] + line[1][0])/2 < canny_seconds.shape[0]/2:
+#                 top_half = True
+#             line_length = sqrt((line[0][0]- line[1][0])**2 + (line[0][1]-line[1][1])**2)
+#             line_lengths = line_lengths + [line_length]
+#             if line_length> max1:
+#                 max1 = line_length
+#                 ang1 = angle
+#                 right_half1 = right_half
+#                 top_half1 = top_half
+#         # axes.plot(origin, (y0, y1), '-r')
+#     ang_seconds = ang1
+#     if ang1>0:
+#         if (top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1 >= deg2rad(45)and ang1 <= deg2rad(90)) or (ang1 >= deg2rad(-90)and ang1<=deg2rad(-45)))):
+#             ang_seconds = ang1
+#         else:
+#             ang_seconds = deg2rad(180) + ang1
+#     else:
+#         if (not top_half1 and ang1 >= deg2rad(-45) and ang1 <= deg2rad(45)) or (right_half1 and ((ang1>=deg2rad(45) and ang1<=deg2rad(90)) or (ang1 >= deg2rad(-90) and ang1 <= deg2rad(-45)))):
+#             ang_seconds = deg2rad(180) + ang1
+#         else:
+#             ang_seconds = deg2rad(360) + ang1
     
-    seconds = round(ang_seconds/deg2rad(360)*60)
-    if max1 == -1:
-        seconds = -1
-    plt.figure(figsize=(12, 9), dpi=80)
-    io.imshow(canny_seconds, cmap='gray')
-    return hours, minutes, seconds
+#     seconds = round(ang_seconds/deg2rad(360)*60)
+#     if max1 == -1:
+#         seconds = -1
+#     plt.figure(figsize=(12, 9), dpi=80)
+#     io.imshow(canny_seconds, cmap='gray')
+#     return hours, minutes, seconds
 
 if __name__ == "__main__":
     img_in = imread('../sekvence/clocks/clock8.jpg')
